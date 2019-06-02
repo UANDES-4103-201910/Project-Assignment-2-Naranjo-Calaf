85ac3e593aaac58c04e55904a78f98baaae6be87
beannama <ben.nar.95@gmail.com>
2019-06-02 18:43:23 -0400

    added separator for posts

--- project/config/routes.rb
+++ project/config/routes.rb
@@ -2,18 +2,18 @@ Rails.application.routes.draw do
   get 'welcome/index'
   devise_for :users
   resources :comments
-  resources :follows
+  # resources :follows
   resources :profiles
-  resources :images
-  resources :attachments
-  resources :links
-  resources :votes
+  # resources :images
+  # resources :attachments
+  # resources :links
+  # resources :votes
   resources :blacklists
   resources :dumpsters
-  resources :innapropiates
+  # resources :innapropiates
   resources :posts
-  resources :admins
-  resources :geofences
+  # resources :admins
+  # resources :geofences
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
