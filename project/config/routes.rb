df8786b1dc8fd5506664c94ab6ddb7da1d97f4e7
beannama <ben.nar.95@gmail.com>
2019-06-02 23:35:10 -0400

    Last Access

--- project/config/routes.rb
+++ project/config/routes.rb
@@ -41,7 +41,6 @@ Rails.application.routes.draw do
 
 
   # TODO: USER ADMINISTRATION: LAST ACCESS ()
-  # TODO: USER ADMINISTRATION: User Role (Query Find id User in Admin Table , if Admin.Super = True SHow SUPER USER, Else: Admin, DEFAULT: User
   # TODO: SYSTEM ADMIN: View Post: Link to : Show posts where the id of the posts = id user logged in
   # TODO: POSTS: Inappropiate Show Posts where Inapropiate.post.id = @post.id)
   # TODO: (X) Tag User
