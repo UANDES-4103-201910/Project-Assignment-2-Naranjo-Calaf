10b559fc92338a27418099264d553682b7aa9545
beannama <ben.nar.95@gmail.com>
2019-06-10 19:54:03 -0400

    Profiles edited

--- project/app/controllers/users/omniauth_callbacks_controller.rb
+++ project/app/controllers/users/omniauth_callbacks_controller.rb
@@ -8,9 +8,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     if @user.persisted?
       flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
-      # crear el profile
-
-      #
       sign_in_and_redirect @user, event: :authentication
     else
       session['devise.google_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
--- project/config/routes.rb
+++ project/config/routes.rb
@@ -23,9 +23,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
-
-  #TODO: Implement authentication with google  (NARANJO) //Failed
-
   devise_scope :user do
 
     authenticated :user do
