daecdbc74479b689376da31a41cb7b28a0aa30e8
beannama <ben.nar.95@gmail.com>
2019-06-10 23:57:25 -0400

    search

--- project/app/models/post.rb
+++ project/app/models/post.rb
@@ -13,4 +13,9 @@ class Post < ApplicationRecord
 
   validates :title, presence: true
   validates :description, presence: true
+
+  def self.search(search)
+    # Title is for the above case, the OP incorrectly had 'name'
+    where("title LIKE ?", "%#{search}%")
+  end
 end
--- project/config/routes.rb
+++ project/config/routes.rb
@@ -36,8 +36,6 @@ Rails.application.routes.draw do
 
 
 
-  # TODO: RUTAS EN POSTS
-  # TODO: USER ADMINISTRATION: LAST ACCESS ()
   # TODO: SYSTEM ADMIN: View Post: Link to : Show posts where the id of the posts = id user logged in
   # TODO: POSTS: Inappropiate Show Posts where Inapropiate.post.id = @post.id)
   # TODO: (X) Tag User
