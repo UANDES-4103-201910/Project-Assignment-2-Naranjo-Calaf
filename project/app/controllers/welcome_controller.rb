9f4bab6d373591ee5b89d19f6b292e06ab3055fe
beannama <ben.nar.95@gmail.com>
2019-07-04 14:53:52 -0400

    Need profile

--- project/app/controllers/welcome_controller.rb
+++ project/app/controllers/welcome_controller.rb
@@ -1,10 +1,14 @@
 class WelcomeController < ApplicationController
   def index
 
+    if(current_user.profile==nil)
+	    redirect_to(new_profile_path)
+    end
     @posts = Post.all.order! 'created_at DESC'
     @user_profile = Profile.where(user_id: current_user.id)
     @userposts = Post.where(user_id: current_user.id)
     @comments = Comment.all.order! 'created_at DESC'
     @votes = Vote.all.order! 'created_at DESC'
   end
+
 end
